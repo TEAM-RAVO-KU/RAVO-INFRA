46b5290821d6857d60ddfa729484c062b867cb62
SangYeon Min <judemin2087@naver.com>
2025-03-18 15:17:00 +0900

    [UPDATE]: docker-compose 설치 스크립트 추가

--- K3s-Host/docker-configuration.sh
+++ K3s-Host/docker-configuration.sh
@@ -7,4 +7,12 @@ dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 # Install Docker and all required dependency packages such as containerd.io
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# set symbolic link and permission
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+
 systemctl enable docker --now
