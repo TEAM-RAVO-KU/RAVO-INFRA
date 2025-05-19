34344b23beaaffc07dbebd6687026b2b1c135886
unknown <judemin2087@naver.com>
2025-05-19 21:39:32 +0900

    [ADD] debezium 배포 및 테스트를 위해 mysql-active 메타데이터를 수정하고 debezium를 정상적으로 배포하여 mysql-active DB에 INSERTS시 Kafka Broker에 메시지가 정상적으로 Produce되고 Kafka CLI를 통해 Consume이 가능한 것을 확인

---
+++ K3s-cluster/debezium/mysql-cdc-debezium.sh
@@ -0,0 +1,62 @@
+kubectl apply -f mysql-cdc-configmap.yaml
+
+# mysql-active Deployment mysql-cdc-config patch
+kubectl -n default patch deploy mysql-active \
+  --type=json \
+  -p='[
+    {"op":"add","path":"/spec/template/spec/volumes/-",
+     "value":{"name":"mysql-cdc","configMap":{"name":"mysql-cdc-config"}}},
+    {"op":"add","path":"/spec/template/spec/containers/0/volumeMounts/-",
+     "value":{"name":"mysql-cdc","mountPath":"/etc/mysql/conf.d/99-debezium.cnf","subPath":"99-debezium.cnf"}}
+  ]'
+
+kubectl rollout restart deployment mysql-active
+
+kubectl exec -it pod/mysql-active-XXXX -- bash
+# ConfigMap 적용 확인
+ls -l /etc/mysql/conf.d/99-debezium.cnf
+cat /etc/mysql/conf.d/99-debezium.cnf
+
+# Debezium 전용 계정 생성
+mysql -u root -p
+<< 'END'
+CREATE USER IF NOT EXISTS 'debezium'@'%' IDENTIFIED BY 'dbz!pass';
+GRANT REPLICATION SLAVE, REPLICATION CLIENT, SELECT, RELOAD, SHOW DATABASES ON *.* TO 'debezium'@'%';
+FLUSH PRIVILEGES;
+END
+
+# 설정 확인
+<< 'END'
+SHOW VARIABLES LIKE 'log_bin';
++---------------+-------+
+| Variable_name | Value |
++---------------+-------+
+| log_bin       | ON    |
++---------------+-------+
+1 row in set (0.010 sec)
+
+SHOW VARIABLES LIKE 'binlog_format';
++---------------+-------+
+| Variable_name | Value |
++---------------+-------+
+| binlog_format | ROW   |
++---------------+-------+
+1 row in set (0.017 sec)
+
+SHOW VARIABLES LIKE 'server_id';
++---------------+--------+
+| Variable_name | Value  |
++---------------+--------+
+| server_id     | 223344 |
++---------------+--------+
+1 row in set (0.009 sec)
+
+SHOW BINARY LOGS;
++------------------+-----------+-----------+
+| Log_name         | File_size | Encrypted |
++------------------+-----------+-----------+
+| mysql-bin.000006 |       158 | No        |
+| mysql-bin.000007 |       923 | No        |
++------------------+-----------+-----------+
+2 rows in set (0.002 sec)
+END
